5c68f4887f5a3259a9e0d9d04f379b6931dc55fd
p5ycod3 <8bit.ds@gmail.com>
2015-10-08 20:50:44 +0300

    added function multiply

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -6,11 +6,13 @@ def subtract(num1, num2)
 	num1 - num2
 end
 
-
 def sum(array =[])
 	sum = 0
 	array.each{|x| sum += x}
 	sum
 end
 
+def multiply(*fvb args)
+	args.inject{|mem, var|mem * var}
+end
 
